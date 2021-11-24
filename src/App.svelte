37d742142a976e00424a3b61998a24f74826734d
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2021-11-24 23:48:39 +0530

    fixed errors

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,9 @@
 <script>
-<<<<<<< HEAD
     import Header from "./UI/Header.svelte";
     import ApodView from "./UI/ApodView.svelte";
-=======
->>>>>>> 5abe28a24d68c70c828aedecb266525d5f737b86
 </script>
 
 <Header />
 <main>
-<<<<<<< HEAD
     <ApodView />
-=======
-	<h1>Astro Park</h1>
-	<p>The place where you can play with the stars!</p>
->>>>>>> 5abe28a24d68c70c828aedecb266525d5f737b86
 </main>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({
-<<<<<<< HEAD
 	target: document.body
-=======
-	target: document.bodys
->>>>>>> 5abe28a24d68c70c828aedecb266525d5f737b86
 });
 
 export default app;
